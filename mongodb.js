d983c5f924b009db8cb15e2e8297ce1d6220c7d2
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-12-23 12:52:53 +0100

    insert test document to mongodb

--- mongodb.js
+++ mongodb.js
@@ -8,5 +8,10 @@ const databaseName = 'task-manager';
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
 	if (error) return console.log('Unable to connect to database');
 
-	console.log('Connected');
+	const db = client.db(databaseName);
+
+	db.collection('users').insertOne({
+		name: 'Dani',
+		age: 23
+	});
 });
