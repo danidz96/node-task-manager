b1b9964a72ec353a2a1bb0080afe3c1355f5c425
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-12-24 02:14:08 +0100

    create user with mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,31 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.LOCAL_DB_URL + 'task-manager-api', { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.LOCAL_DB_URL, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+const User = mongoose.model('User', {
+	name: {
+		type: String
+	},
+	age: {
+		type: Number
+	}
+});
+
+const me = new User({
+	name: 'Dani',
+	age: 23
+});
+
+me
+	.save()
+	.then(() => {
+		console.log(me);
+	})
+	.catch((error) => {
+		console.log('Error!', error);
+	});
