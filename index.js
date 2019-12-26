e9ff9150bbbd6917366cc0d19e95b7f934d6682d
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-12-26 15:37:09 +0100

    add reading users endpoints

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+	User.find()
+		.then((users) => {
+			res.send(users);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+app.get('/users/:id', (req, res) => {
+	const id = req.params.id;
+
+	User.findById(id)
+		.then((user) => {
+			res.send(user);
+		})
+		.catch((error) => {
+			res.status(404).send();
+		});
+});
+
 app.post('/users', (req, res) => {
 	const user = new User(req.body);
 
