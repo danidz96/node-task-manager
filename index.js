20d449212315ccf2c894e6242073990d6636c7f2
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-12-26 13:48:06 +0100

    add enpoint for create users

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+require('./src/db/mongoose');
+const User = require('./src/models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+	const user = new User(req.body);
+
+	user
+		.save()
+		.then(() => {
+			res.send(user);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
+});
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
