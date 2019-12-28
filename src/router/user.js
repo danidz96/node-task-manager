8eeaea869485617949ee61980d5779e858fcfbce
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-12-28 13:39:34 +0100

    add user login

--- src/router/user.js
+++ src/router/user.js
@@ -32,6 +32,15 @@ router.post('/users', async (req, res) => {
 	}
 });
 
+router.post('/users/login', async (req, res) => {
+	try {
+		const user = await User.findByCredentials(req.body.email, req.body.password);
+		res.send(user);
+	} catch (error) {
+		res.status(400).send();
+	}
+});
+
 router.patch('/users/:id', async (req, res) => {
 	const id = req.params.id;
 	const updates = Object.keys(req.body);
