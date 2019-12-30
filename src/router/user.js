2f502e04c1073fccaa60c97c18884bfd736cbdd8
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-12-30 13:17:24 +0100

    add middleware for authentication

--- src/router/user.js
+++ src/router/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
 	try {
-		const users = await User.find();
-		res.send(users);
+		res.send(req.user);
 	} catch (error) {
 		res.status(500).send(error);
 	}
