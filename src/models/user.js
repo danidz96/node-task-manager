08246d8656feed19edcdc152cceb5946ac3355d9
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-02 14:47:12 +0100

    implement user/task relationship

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,12 @@ const userSchema = new mongoose.Schema({
 	]
 });
 
+userSchema.virtual('tasks', {
+	ref: 'Task',
+	localField: '_id',
+	foreignField: 'owner'
+});
+
 userSchema.statics.findByCredentials = async (email, password) => {
 	const user = await User.findOne({ email });
 
