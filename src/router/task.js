c7765faad141e602b6d9d90c25e1e4b356a1a682
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-03 13:15:50 +0100

    get all tasks for logged user

--- src/router/task.js
+++ src/router/task.js
@@ -3,10 +3,13 @@ const router = new express.Router();
 const Task = require('../models/task');
 const auth = require('../middleware/auth');
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
 	try {
-		const tasks = await Task.find();
-		res.send(tasks);
+		// const tasks = await Task.find({ owner: req.user._id });
+		// res.send(tasks);
+
+		await req.user.populate('tasks').execPopulate();
+		res.send(req.user.tasks);
 	} catch (error) {
 		res.status(500).send();
 	}
