82f0c28e1f998940e6ac1ddee32ba7e301263312
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-03 13:08:05 +0100

    add authentication to get task by id and owner

--- src/router/task.js
+++ src/router/task.js
@@ -12,14 +12,17 @@ router.get('/tasks', async (req, res) => {
 	}
 });
 
-router.get('/tasks/:id', async (req, res) => {
-	const id = req.params.id;
+router.get('/tasks/:id', auth, async (req, res) => {
+	const _id = req.params.id;
 
 	try {
-		const task = await Task.findById(id);
+		const task = await Task.findOne({ _id, owner: req.user._id });
+		if (!task) {
+			return res.status(404).send();
+		}
 		res.send(task);
 	} catch (error) {
-		res.status(404).send();
+		res.status(500).send();
 	}
 });
 
@@ -36,7 +39,7 @@ router.post('/tasks', auth, async (req, res) => {
 	}
 });
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
 	const id = req.params.id;
 	const updates = Object.keys(req.body);
 	const allowedUpdates = [ 'description', 'completed' ];
