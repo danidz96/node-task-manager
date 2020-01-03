2a3a305a416329cb68fefae842b2d46860e9b08b
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-03 14:49:20 +0100

    remove all tasks from user when it is deleted

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -96,6 +97,14 @@ userSchema.pre('save', async function(next) {
 	if (user.isModified('password')) {
 		user.password = await bcrypt.hash(user.password, 8);
 	}
+
+	next();
+});
+
+userSchema.pre('remove', async function(next) {
+	const user = this;
+
+	await Task.deleteMany({ owner: user._id });
 
 	next();
 });
