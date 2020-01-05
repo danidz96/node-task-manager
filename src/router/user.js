700d0be5489ccbd0bc1f14734af23279218726c6
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-05 22:04:08 +0100

    validate image uploads

--- src/router/user.js
+++ src/router/user.js
@@ -13,7 +13,17 @@ router.get('/users/me', auth, async (req, res) => {
 });
 
 const upload = multer({
-	dest: 'avatars'
+	dest: 'avatars',
+	limits: {
+		fileSize: 1000000
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+			return cb(new Error('Please upload an image'));
+		}
+
+		cb(null, true);
+	}
 });
 
 router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
