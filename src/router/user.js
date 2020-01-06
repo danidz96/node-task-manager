6e58c3d0e8a82a8db53b5765a0e780dfb1c8bd0b
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-06 13:43:50 +0100

    resize avatar and convert to png with sharp

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
@@ -33,7 +34,7 @@ router.get('/users/:id/avatar', async (req, res) => {
 			throw new Error();
 		}
 
-		res.set('Content-type', 'image/jpg');
+		res.set('Content-type', 'image/png');
 		res.send(user.avatar);
 	} catch (error) {
 		res.status(404).send();
@@ -45,7 +46,8 @@ router.post(
 	auth,
 	upload.single('avatar'),
 	async (req, res) => {
-		req.user.avatar = req.file.buffer;
+		const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+		req.user.avatar = buffer;
 		await req.user.save();
 		res.send();
 	},
