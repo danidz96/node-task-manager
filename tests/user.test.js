d4eae34ed3947339eadf22e540bbc16e1f3e4f3c
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-12 19:48:07 +0100

    add login user test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+	name: 'Mireia',
+	email: 'mireia@email.com',
+	password: 'pass1234'
+};
+
+beforeEach(async () => {
+	await User.deleteMany();
+	await new User(userOne).save();
+});
 
 test('Should signup a new user', async () => {
 	await request(app)
@@ -10,4 +22,15 @@ test('Should signup a new user', async () => {
 			password: '123456789'
 		})
 		.expect(201);
+});
+
+test('Should login existing user', async () => {
+	await request(app).post('/users/login').send({ email: userOne.email, password: userOne.password }).expect(200);
+});
+
+test('Should not login nonexisting user', async () => {
+	await request(app)
+		.post('/users/login')
+		.send({ email: 'nonexistingmal@email.com', password: 'fakepassword' })
+		.expect(400);
 });
