17b62225a7512b31d7e6af7f2773135e03438d35
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-13 19:51:59 +0100

    add test for delete user account

--- tests/user.test.js
+++ tests/user.test.js
@@ -47,3 +47,7 @@ test('Should get profile for user', async () => {
 test('Should not get profile for unauthenticated user', async () => {
 	await request(app).get('/users/me').send().expect(401);
 });
+
+test('Should delete account for user', async () => {
+	await request(app).delete('/users/me').set('Authorization', `Bearer ${userOne.tokens[0].token}`).send().expect(200);
+});
