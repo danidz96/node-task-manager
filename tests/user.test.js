7d44d932190d233b34d35258e13da844ee915d06
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-13 19:49:04 +0100

    add test for unauthenticated user

--- tests/user.test.js
+++ tests/user.test.js
@@ -43,3 +43,7 @@ test('Should not login nonexisting user', async () => {
 test('Should get profile for user', async () => {
 	await request(app).get('/users/me').set('Authorization', `Bearer ${userOne.tokens[0].token}`).send().expect(200);
 });
+
+test('Should not get profile for unauthenticated user', async () => {
+	await request(app).get('/users/me').send().expect(401);
+});
