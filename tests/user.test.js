2fa0a488b450b9d4a7b5161c27583dd9258a0af4
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-15 23:03:20 +0100

    add assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -44,7 +44,13 @@ test('Should signup a new user', async () => {
 });
 
 test('Should login existing user', async () => {
-	await request(app).post('/users/login').send({ email: userOne.email, password: userOne.password }).expect(200);
+	const response = await request(app)
+		.post('/users/login')
+		.send({ email: userOne.email, password: userOne.password })
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('Should not login nonexisting user', async () => {
@@ -64,6 +70,9 @@ test('Should not get profile for unauthenticated user', async () => {
 
 test('Should delete account for user', async () => {
 	await request(app).delete('/users/me').set('Authorization', `Bearer ${userOne.tokens[0].token}`).send().expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user).toBeNull();
 });
 
 test('Should not delete account for unauthenticated user', async () => {
