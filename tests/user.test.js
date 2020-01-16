861c157567d1ddaff077185c11eeefa1c86295d1
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-16 22:21:27 +0100

    add user avatr tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -78,3 +78,14 @@ test('Should delete account for user', async () => {
 test('Should not delete account for unauthenticated user', async () => {
 	await request(app).delete('/users/me').send().expect(401);
 });
+
+test('Should upload avatar image', async () => {
+	await request(app)
+		.post('/users/me/avatar')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.attach('avatar', 'tests/fixtures/logo.png')
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user.avatar).toEqual(expect.any(Buffer));
+});
