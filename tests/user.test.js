2caae971ca17e8c204ee8c061b305d684a8664b4
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-17 22:52:25 +0100

    add test for update user

--- tests/user.test.js
+++ tests/user.test.js
@@ -89,3 +89,14 @@ test('Should upload avatar image', async () => {
 	const user = await User.findById(userOneId);
 	expect(user.avatar).toEqual(expect.any(Buffer));
 });
+
+test('Should update valid user fields', async () => {
+	await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({ name: 'Mireia' })
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user.name).toEqual('Mireia');
+});
