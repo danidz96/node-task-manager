7a4b19082d7592edc7f5ad3dd224ae4c31d66e6a
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-19 19:33:25 +0100

    add new user to test db

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const User = require('../../src/models/user');
+const Task = require('../../src/models/task');
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -11,13 +12,25 @@ const userOne = {
 	tokens: [ { token: jwt.sign({ _id: userOneId }, process.env.SECRET_KEY) } ]
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+	_id: userTwoId,
+	name: 'Dani',
+	email: 'dani@gmail.com',
+	password: '123456789',
+	tokens: [ { token: jwt.sign({ _id: userTwoId }, process.env.SECRET_KEY) } ]
+};
+
 const setupDatabase = async () => {
 	await User.deleteMany();
 	await new User(userOne).save();
+	await new User(userTwo).save();
 };
 
 module.exports = {
 	userOne,
 	userOneId,
+	userTwo,
+	userTwoId,
 	setupDatabase
 };
